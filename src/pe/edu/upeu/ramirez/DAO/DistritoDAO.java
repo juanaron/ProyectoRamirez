34c6affcf5528a71fd010ca8d8647bdbf7a3ee62
alum.fial7 <alum.fial7@FIA-LAB7B4.lim.upeu.edu.pe>
2014-06-25 15:28:19 -0500

    VERSION 4

--- src/pe/edu/upeu/ramirez/DAO/DistritoDAO.java
+++ src/pe/edu/upeu/ramirez/DAO/DistritoDAO.java
@@ -6,10 +6,46 @@
 
 package pe.edu.upeu.ramirez.DAO;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import pe.edu.upeu.ramirez.config.Conexion;
+import pe.edu.upeu.ramirez.modelo.Distrito;
+
 /**
  *
  * @author alum.fial7
  */
 public class DistritoDAO {
+    private Connection cx ;
+    private Statement st ;
+    private ResultSet rs;
+    private String sql;
+    ArrayList<Distrito> lista  = null;
+    public ArrayList<Distrito> listarDistrito(){
+        lista = new ArrayList();
+        sql = "SELECT * FROM distrito";
+        try{
+        
+                  cx = Conexion.getConex();
+                  st = cx.createStatement();
+                  rs  = st.executeQuery(sql);
+     while(rs.next()){
+            Distrito d = new Distrito();
+            d.setIdd(rs.getInt("iddistrito"));
+            d.setIdr(rs.getInt("idregion"));
+            d.setDisc(rs.getString("distrito"));
+            d.setEstado(rs.getString("estado"));
+            lista.add(d);
+        }
+                          }  
+        catch(SQLException e ){
+            
+        }
+        return lista;
+        
+    }
     
 }
--- src/pe/edu/upeu/ramirez/DAO/TipoIglesiaDAO.java
+++ src/pe/edu/upeu/ramirez/DAO/TipoIglesiaDAO.java
@@ -6,10 +6,45 @@
 
 package pe.edu.upeu.ramirez.DAO;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import pe.edu.upeu.ramirez.config.Conexion;
+import pe.edu.upeu.ramirez.modelo.Distrito;
+import pe.edu.upeu.ramirez.modelo.TipoIglesia;
+
 /**
  *
  * @author alum.fial7
  */
 public class TipoIglesiaDAO {
+    private Connection cx ;
+    private Statement st ;
+    private ResultSet rs;
+    private String sql;
+    ArrayList<TipoIglesia> lista  = null;
+    public ArrayList<TipoIglesia> listarTipoIglesia(){
+        lista = new ArrayList();
+        sql = "SELECT * FROM tipo_iglesia";
+        try{
+        
+                  cx = Conexion.getConex();
+                  st = cx.createStatement();
+                  rs  = st.executeQuery(sql);
+     while(rs.next()){
+            TipoIglesia ti = new TipoIglesia();
+            ti.setIdti(rs.getInt("idtipo_iglesia"));
+            ti.setNomtipo(rs.getString("tipo_iglesia"));
+            lista.add(ti);
+        }
+                          }  
+        catch(SQLException e ){
+            
+        }
+        return lista;
+        
+    }
     
 }
